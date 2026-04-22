9f5098770537c78a5ec45284462d1d4eba050a50
Dhafiz93 <saibouabdoulhafiz@gmail.com>
2026-04-22 15:32:35 +0000

    Ajout d'une citation

--- app.py
+++ app.py
@@ -199,6 +199,7 @@ DEFAULT_QUOTES = [
     "Ne jamais abandonner, même quand le chemin semble difficile.",
     "La discipline est le pont entre les objectifs et les accomplissements.",
     "Le plus grand risque est de ne prendre aucun risque.",
+    "La seule limite à notre épanouissement de demain sera nos doutes d'aujourd'hui.",
 ]
 
 
